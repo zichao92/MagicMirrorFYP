fc805aa88db1239e516dfec5c063652c0bed9795
ZiChao <vettrazc@gmail.com>
2017-03-15 22:18:20 +0800

    Test 6
    Added Remote Control module & Youtube

---
+++ Modules/MMM-Remote-Control/MMM-Remote-Control.js
@@ -0,0 +1,238 @@
+/* global Module, Log, MM, config */
+
+/* Magic Mirror
+ * Module: Remote Control
+ *
+ * By Joseph Bethge
+ * MIT Licensed.
+ */
+
+Module.register("MMM-Remote-Control", {
+
+	requiresVersion: "2.0.0",
+
+	// Default module config.
+	defaults: {
+		// no config options at the moment
+	},
+
+	// Define start sequence.
+	start: function() {
+		Log.info("Starting module: " + this.name);
+
+		this.settingsVersion = 1;
+
+		this.addresses = [];
+
+		this.brightness = 100;
+	},
+
+	getStyles: function() {
+		return ["remote-control.css"];
+	},
+
+	notificationReceived: function(notification, payload, sender) {
+		if (sender) {
+			Log.log(this.name + " received a module notification: " + notification + " from sender: " + sender.name);
+			if (notification === "REMOTE_ACTION") {
+				this.sendSocketNotification(notification, payload);	
+			}
+		} else { 
+			if (notification === "DOM_OBJECTS_CREATED") {
+				this.sendSocketNotification("LANG", config.language);
+				this.sendSocketNotification("REQUEST_DEFAULT_SETTINGS");
+			}
+		}
+	},
+
+	// Override socket notification handler.
+	socketNotificationReceived: function(notification, payload) {
+		if (notification === "UPDATE") {
+			this.sendCurrentData();
+		}
+		if (notification === "IP_ADDRESSES") {
+			this.addresses = payload;
+			if (this.data.position)
+			{
+				this.updateDom();
+			}
+		}
+		if (notification === "DEFAULT_SETTINGS") {
+			var settingsVersion = payload.settingsVersion;
+
+			if (settingsVersion === undefined) {
+				settingsVersion = 0;
+			}
+			if (settingsVersion < this.settingsVersion) {
+				if (settingsVersion === 0) {
+					// move old data into moduleData
+					payload = { moduleData : payload, brightness : 100};
+				}
+			}
+
+			var moduleData = payload.moduleData;
+			var modules = MM.getModules();
+			for (var k = 0; k < moduleData.length; k++) {
+				for (var i = 0; i < modules.length; i++) {
+					if (modules[i].identifier === moduleData[k].identifier) {
+						if (moduleData[k].hidden) {
+							modules[i].hide();
+						}
+					}
+				}
+			}
+			this.setBrightness(payload.brightness);
+		}
+		if (notification === "BRIGHTNESS") {
+			this.setBrightness(parseInt(payload));
+		}
+		if (notification === "HIDE" || notification === "SHOW") {
+			var options = {lockString: this.identifier};
+			var modules = MM.getModules();
+			for (var i = 0; i < modules.length; i++) {
+				if (modules[i].identifier === payload.module) {
+					if (notification === "HIDE") {
+						if (payload.force) {
+							options.force = true;
+						}
+						modules[i].hide(1000, options);
+					} else {
+						modules[i].show(1000, options);
+					}
+				}
+			}
+		}
+		if (notification === "NOTIFICATION") {
+			this.sendNotification(payload.notification, payload.payload);
+		}
+	
+	},
+
+	buildCssContent: function(brightness) {
+		var css = "";
+
+		var defaults = {
+			"body": parseInt("aa", 16),
+			"header": parseInt("99", 16),
+			".dimmed": parseInt("66", 16),
+			".normal": parseInt("99", 16),
+			".bright": parseInt("ff", 16)
+		}
+
+		for (var key in defaults) {
+			var value = defaults[key] / 100 * brightness;
+			value = Math.round(value);
+			value = Math.min(value, 255);
+			if (value < 16)
+			{
+				value = "0" + value.toString(16);
+			} else {
+				value = value.toString(16);
+			}
+			var extra = "";
+			if (key === "header") {
+				extra = "border-bottom: 1px solid #" + value + value + value + ";"
+			}
+			css += key + " { color: #" + value + value + value + "; " + extra + "} ";
+		}
+		return css;
+	},
+
+	setBrightness: function(newBrightnessValue) {
+		if (newBrightnessValue < 10) {
+			newBrightnessValue = 10;
+		}
+		if (newBrightnessValue > 200) {
+			newBrightnessValue = 200;
+		}
+
+		this.brightness = newBrightnessValue;
+
+		var style = document.getElementById('remote-control-styles');
+		if (!style) {
+			// create custom css if not existing
+			style = document.createElement('style');
+			style.type = 'text/css';
+			style.id = 'remote-control-styles';
+			var parent = document.getElementsByTagName('head')[0];
+			parent.appendChild(style);
+		}
+
+		if (newBrightnessValue < 100) {
+			style.innerHTML = "";
+			this.createOverlay(newBrightnessValue);
+			return;
+		}
+		if (newBrightnessValue > 100) {
+			style.innerHTML = this.buildCssContent(newBrightnessValue);
+			this.removeOverlay();
+			return;
+		}
+		// default brightness
+		style.innerHTML = "";
+		this.removeOverlay();
+	},
+
+	createOverlay: function(brightness) {
+		var overlay = document.getElementById('remote-control-overlay');
+		if (!overlay) {
+			// if not existing, create overlay
+			var overlay = document.createElement("div");
+			overlay.id = "remote-control-overlay";
+			var parent = document.body;
+			parent.insertBefore(overlay, parent.firstChild);
+		}
+		var bgColor = "rgba(0,0,0," + (100 - brightness)/100 + ")";
+		overlay.style.backgroundColor = bgColor;
+	},
+
+	removeOverlay: function() {
+		var overlay = document.getElementById('remote-control-overlay');
+		if (overlay) {
+			var parent = document.body;
+			parent.removeChild(overlay);
+		}
+	},
+
+	getDom: function() {
+		var wrapper = document.createElement("div");
+		if (this.addresses.length === 0) {
+			this.addresses = ["ip-of-your-mirror"];
+		}
+		wrapper.innerHTML = "http://" + this.addresses[0] + ":8080/remote.html";
+		wrapper.className = "normal xsmall";
+		return wrapper;
+	},
+
+	removeOwnLockString: function(lockStrings) {
+		if (!lockStrings) {
+			return [];
+		}
+		var newLockStrings = [];
+		for (var i = 0; i < lockStrings.length; i++) {
+			if (lockStrings[i] !== this.id) {
+				newLockStrings.push(lockStrings[i]);
+			}
+		}
+		return newLockStrings;
+	},
+
+	sendCurrentData: function() {
+		var modules = MM.getModules();
+		var currentModuleData = [];
+		for (var i = 0; i < modules.length; i++) {
+			currentModuleData.push({});
+			currentModuleData[i]["hidden"] = modules[i].hidden;
+			currentModuleData[i]["lockStrings"] = this.removeOwnLockString(modules[i].lockStrings);
+			currentModuleData[i]["name"] = modules[i].name;
+			currentModuleData[i]["identifier"] = modules[i].identifier;
+			currentModuleData[i]["position"] = modules[i].data.position;
+		}
+		var configData = {
+			moduleData: currentModuleData,
+			brightness: this.brightness,
+			settingsVersion: this.settingsVersion
+		};
+		this.sendSocketNotification("CURRENT_STATUS", configData);
+	},
+});
---
+++ Modules/MMM-Remote-Control/README.md
@@ -0,0 +1,112 @@
+# Magic Mirror Module: Remote Control
+
+This module for the [Magic Mirror²](https://github.com/MichMich/MagicMirror) allows you to quickly shutdown your mirror through a web browser.
+The website should work fine on any device (desktop, smart phone, tablet, ...).
+Since we all want our [SD cards to live a long and prosper life](http://raspberrypi.stackexchange.com/a/383) we properly shut down before pulling the power plug everytime, am I right?
+Additionally you can hide and show modules on your mirror.
+
+![The Main Menu](.github/main.png)
+![The Power Menu](.github/power.png)
+![Hide and Show a Module](.github/hide_show_module.gif)
+
+## Installation
+
+- (1) Clone this repository in your `MagicMirror/modules` folder:
+```bash
+git clone https://github.com/Jopyth/MMM-Remote-Control.git
+```
+
+- (2) Add the module to your `config/config.js` file, if you add a `position`, it will display the URL to the remote on the mirror.
+```javascript
+{
+    module: 'MMM-Remote-Control'
+    // uncomment the following line to show the URL of the remote control on the mirror
+    // , position: 'bottom_left'
+    // you can hide this module afterwards from the remote control itself
+},
+```
+
+- (3) Access the remote interface on [http://192.168.xxx.xxx:8080/remote.html](http://192.168.xxx.xxx:8080/remote.html).
+
+- (4) If you are not running with `sudo` rights, the shutdown does not work (it *should* work for everyone who did not change anything on this matter).
+
+## Update
+
+Update this module by navigating into its folder on the command line and executing this command: `git pull`.
+
+## Call methods from other modules
+
+You can call any of the methods provided in the UI directly through a GET request, or a module notification.
+For example you can use [MMM-ModuleScheduler](https://forum.magicmirror.builders/topic/691/mmm-modulescheduler) to automatically shutdown your RasberryPi at a certain time, or integrate it with home automation systems.
+
+### Examples
+
+- Example for a GET request to trigger a RaspberryPi restart:
+```
+http://192.168.xxx.xxx:8080/remote?action=RESTART
+```
+
+- Example for a notification schedule for [MMM-ModuleScheduler](https://forum.magicmirror.builders/topic/691/mmm-modulescheduler) to automatically switch your monitor on and off with :
+```javascript
+notification_schedule: [
+    {notification: 'REMOTE_ACTION', schedule: '30 9 * * *', payload: {action: 'MONITOROFF'}},
+    {notification: 'REMOTE_ACTION', schedule: '30 18 * * *', payload: {action: 'MONITORON'}}
+]
+```
+
+- Example to trigger a RaspberryPi restart in your module:
+```
+this.sendNotification('REMOTE_ACTION', {action: 'RESTART'});
+```
+
+### List of actions
+
+| action | description |
+| ------------- | ------------- |
+| SHUTDOWN | Shutdown your RaspberryPi |
+| REBOOT | Restart your RaspberryPi |
+| RESTART | Restart your MagicMirror |
+| MONITORON | Switch your display on |
+| MONITOROFF | Switch your display off |
+| SAVE | Save the current configuration (show and hide status of modules, and brightness), will be applied after the mirror starts |
+| BRIGHTNESS | Change mirror brightness, with the new value specified by `value`. `100` equals the default, possible range is between `10` and `200`. |
+| HIDE | Hide a module, with the identifier specified by `module` (see `MODULE_DATA` action). |
+| SHOW | Show a module, with the identifier specified by `module` (see `MODULE_DATA` action). |
+| MODULE_DATA | Returns a JSON format of the data displayed in the UI, including all valid identifiers for the `HIDE` and `SHOW` action. |
+
+### Format of module data response
+
+The response will be in the JSON format, here is an example:
+
+```json
+{
+"moduleData":[
+    {"hidden":false,"name":"alert","identifier":"module_0_alert"},
+    {"hidden":true,"name":"clock","identifier":"module_1_clock","position":"bottom_right"},
+    {"hidden":false,"name":"currentweather","identifier":"module_2_currentweather","position":"top_right"}
+],
+"brightness":40,
+"settingsVersion":1
+}
+```
+
+## License
+
+The MIT License (MIT)
+=====================
+
+Copyright © 2016 Joseph Bethge
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the “Software”), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+**The software is provided “as is”, without warranty of any kind, express or implied, including but not limited to the warranties of merchantability, fitness for a particular purpose and noninfringement. In no event shall the authors or copyright holders be liable for any claim, damages or other liability, whether in an action of contract, tort or otherwise, arising from, out of or in connection with the software or the use or other dealings in the software.**
